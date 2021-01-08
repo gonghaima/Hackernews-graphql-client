5eacb919e8372eb20e5fd0456a5db10185a8a4b2
Steven <gonghaima@hotmail.com>
2021-01-08 12:21:59 +1100

    subscribeToMore - minimum code to get it working

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -12,8 +12,39 @@ const FEED_QUERY = gql`
     }
 `;
 
+const FEED_SUBSCRIPTION = gql`
+    subscription {
+        newLink {
+            id
+            description
+            url
+            postedBy {
+                name
+                email
+            }
+            votes {
+                link {
+                    url
+                    description
+                }
+            }
+        }
+    }
+`;
+
 const LinkList = () => {
-    const { data } = useQuery(FEED_QUERY);
+    const { subscribeToMore, data } = useQuery(FEED_QUERY);
+    subscribeToMore({
+        document: FEED_SUBSCRIPTION,
+        updateQuery: (prev, { subscriptionData }) => {
+            if (!subscriptionData.data) return prev;
+            // this updateQuery triggered twice somehow, check if the new data has been included before merge in again.
+            if (prev.feed.map(fd => fd.id).includes(subscriptionData.data.newLink.id)) return prev;
+            return Object.assign({}, prev, {
+                feed: [subscriptionData.data.newLink, ...prev.feed]
+            });
+        }
+    });
     return (
         <div>
             {data && (
